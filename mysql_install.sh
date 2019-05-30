37f6a78abfa4a9b1d660c9c9c599db7410eab046
小布丁的心事 <kuku66ma@gmail.com>
2019-05-30 11:07:54 +0800

    Create mysql_install.sh

---
+++ mysql_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#=================================================
+#	System Required: CentOS 7
+#	Description: Install the mysql and configurion
+#	Version: 1.0.1"
+#	Author: 小布丁的心事
+#=================================================
+sh_ver="1.0.1"
